2761e93e6bda2ea214836c6aeb16cf4fb12689d1
Evan Sosenko <evan@getseam.com>
2023-04-13 16:36:20 -0700

    Add test for multiple calls

--- test/hoist.test.ts
+++ test/hoist.test.ts
@@ -11,6 +11,9 @@ test('hoistMethods: moves methods to top level', (t) => {
       fur: true,
       shaveDog() {
         set({ fur: false })
+      },
+      unshaveDog() {
+        set({ fur: true })
       }
     }))
   )
@@ -29,6 +32,41 @@ test('hoistMethods: moves methods to top level', (t) => {
   t.false(store.getState().fur, 'has updated state')
 })
 
+test('hoistMethods: handles multiple state updates', (t) => {
+  const store = hoistMethods<StoreApi<State & Methods>>(
+    createStore((set) => ({
+      paw: true,
+      snout: true,
+      fur: true,
+      shaveDog() {
+        set({ fur: false })
+      },
+      unshaveDog() {
+        set({ fur: true })
+      }
+    }))
+  )
+
+  t.true(store.getState().fur, 'has initial state')
+  store.shaveDog()
+  t.false(store.getState().fur, 'has updated state')
+  store.unshaveDog()
+  t.true(store.getState().fur, 'has next updated state')
+  store.shaveDog()
+  store.shaveDog()
+  t.false(store.getState().fur, 'has updated state two calls')
+  store.unshaveDog()
+  store.shaveDog()
+  store.shaveDog()
+  store.shaveDog()
+  store.unshaveDog()
+  store.shaveDog()
+  store.shaveDog()
+  store.unshaveDog()
+  store.unshaveDog()
+  t.true(store.getState().fur, 'has updated state after mutiple calls')
+})
+
 interface State {
   paw: boolean
   snout: boolean
@@ -37,4 +75,5 @@ interface State {
 
 interface Methods {
   shaveDog: () => void
+  unshaveDog: () => void
 }
