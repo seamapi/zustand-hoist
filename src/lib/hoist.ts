19be429cd9ce500f3c8ead2749406fd9652585ab
Evan Sosenko <evan@getseam.com>
2023-04-20 17:33:07 -0700

    feat: Add hoistState

--- src/lib/hoist.ts
+++ src/lib/hoist.ts
@@ -1,5 +1,32 @@
 import type { StoreApi } from 'zustand/vanilla'
 
+type StateOnly<T> = {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  [K in keyof T]: T[K] extends Function ? never : T[K]
+}
+
+export const hoistState = <T extends StoreApi<any> = StoreApi<any>>(
+  store: T
+): T & StateOnly<ReturnType<T['getState']>> => {
+  const state = store.getState()
+  const keys = Object.keys(state)
+
+  const newStore = keys.reduce((prevStore, key) => {
+    const value = state[key]
+    if (typeof value === 'function') return prevStore
+    return {
+      ...prevStore,
+      get [key]() {
+        return store.getState()[key]
+      }
+    }
+  }, store)
+
+  return newStore as T & ActionsOnly<ReturnType<T['getState']>>
+}
+
+export type HoistedStateStoreApi<T> = StoreApi<T> & StateOnly<T>
+
 type ActionsOnly<T> = {
   // eslint-disable-next-line @typescript-eslint/ban-types
   [K in keyof T]: T[K] extends Function ? T[K] : never
