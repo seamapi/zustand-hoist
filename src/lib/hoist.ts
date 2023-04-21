cd5e26c74da3ca294dcc1daba8ce1594b1e6c7c7
Evan Sosenko <evan@getseam.com>
2023-04-21 00:08:14 -0700

    Fix type casts

--- src/lib/hoist.ts
+++ src/lib/hoist.ts
@@ -1,14 +1,12 @@
 import type { StoreApi } from 'zustand/vanilla'
 
-import { hoistActions, type HoistedActionStoreApi } from './hoist-actions.js'
-import { type HoistedStateStoreApi, hoistState } from './hoist-state.js'
+import { hoistActions } from './hoist-actions.js'
+import { hoistState } from './hoist-state.js'
 
 export const hoist = <T extends StoreApi<any> = StoreApi<any>>(
   store: T
-): T & HoistedStateStoreApi<T> & HoistedActionStoreApi<T> => {
-  return hoistState(hoistActions(store))
+): T & ReturnType<T['getState']> => {
+  return hoistState(hoistActions(store)) as T & ReturnType<T['getState']>
 }
 
-export type HoistedStoreApi<T> = StoreApi<T> &
-  HoistedStateStoreApi<T> &
-  HoistedActionStoreApi<T>
+export type HoistedStoreApi<T> = StoreApi<T> & T
